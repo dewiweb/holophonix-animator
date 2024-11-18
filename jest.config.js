c2234093a635b72585e512cf5a0e60c2865ae576
dewiweb <dewiweb@gmail.com>
2024-11-18 20:11:32 +0100

    build: add Jest testing infrastructure
    - Add Jest and TypeScript testing dependencies
    - Configure Jest for TypeScript support
    - Add test scripts to package.json
    - Remove package-lock.json in favor of yarn.lock

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
