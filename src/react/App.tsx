0c2bfa72f81ac0c6841951919a8961ff8ebe4a6f
dewiweb <dewiweb@gmail.com>
2024-11-27 21:34:33 +0100

    feat: Update React frontend structure and components
    - Add new React components
    - Add context providers
    - Add custom hooks
    - Update package dependencies

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import MainLayout from './components/layouts/MainLayout';
 import Home from './pages/Home';
 import Settings from './pages/Settings';
+import { GroupManager } from '../bindings';
+import { AppProvider } from './context/AppContext';
+import { GroupTreeView } from './components/groups/GroupTreeView';
+import '@blueprintjs/core/lib/css/blueprint.css';
 import './styles/global.css';
+import './App.css';
 
 const App: React.FC = () => {
+  const [selectedGroupId, setSelectedGroupId] = useState<string>();
+  const groupManager = new GroupManager();
+
   return (
     <Provider store={store}>
       <Router>
         <MainLayout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
+          <AppProvider groupManager={groupManager}>
+            <div className="app-container">
+              <div className="sidebar">
+                <GroupTreeView 
+                  selectedGroupId={selectedGroupId}
+                  onGroupSelect={setSelectedGroupId}
+                />
+              </div>
+              <div className="main-content">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/settings" element={<Settings />} />
+                </Routes>
+              </div>
+            </div>
+          </AppProvider>
         </MainLayout>
       </Router>
     </Provider>
